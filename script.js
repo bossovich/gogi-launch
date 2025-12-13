20b72e07d341bc07325ff461b97229fb6d724491
bossovich <petrugogu05@gmail.com>
2025-12-13 11:17:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("GOGI Launchpad Loaded Successfully");
+const sidebar = document.getElementById("sidebar");
+const backdrop = document.getElementById("sidebarBackdrop");
+
+document.getElementById("openSidebar").onclick = () => {
+    sidebar.style.left = "0";
+    backdrop.style.display = "block";
+};
+
+document.getElementById("closeSidebar").onclick = () => {
+    sidebar.style.left = "-320px";
+    backdrop.style.display = "none";
+};
+
+backdrop.onclick = () => {
+    sidebar.style.left = "-320px";
+    backdrop.style.display = "none";
+};
