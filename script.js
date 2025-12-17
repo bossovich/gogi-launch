6d0a912fadfbbfe6ce0621e191aaa8d60c788d29
bossovich <petrugogu05@gmail.com>
2025-12-17 18:40:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const sidebar = document.getElementById("sidebar");
-const main = document.getElementById("main");
+const page = document.getElementById("page");
 const toggle = document.getElementById("toggleSidebar");
-const why = document.querySelector(".why");
-const hinkali = document.querySelector(".hero-right img");
 
 toggle.onclick = () => {
   sidebar.classList.toggle("open");
-  sidebar.classList.toggle("collapsed");
-  main.style.transform = sidebar.classList.contains("open")
-    ? "scale(.95) translateX(120px)"
-    : "none";
-};
-
-why.onclick = () => {
-  hinkali.style.transform = "translateX(-14px) scale(1.03)";
-  setTimeout(()=>hinkali.style.transform="",420);
+  page.classList.toggle("shrink");
 };
