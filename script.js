3bca0fe0838eed0bb8d086b1817bd3cf8bc7ff32
bossovich <petrugogu05@gmail.com>
2025-12-18 14:51:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const sidebar = document.getElementById("sidebar");
-const burger = document.getElementById("burger");
+const sidebar = document.querySelector('.sidebar');
+const burger = document.querySelector('.hamburger');
 
-burger.onclick = () => {
-  sidebar.classList.toggle("collapsed");
-};
+burger.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
+
+const why = document.getElementById('whyUs');
+const hinkali = document.getElementById('hinkali');
+
+why.addEventListener('mouseenter', () => {
+  hinkali.classList.add('animate');
+});
+
+why.addEventListener('mouseleave', () => {
+  hinkali.classList.remove('animate');
+});
